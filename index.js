a53ec31be54c288ce42f3e96c874c4dc7f260fd6
shudhanidhisonii <155170720+shudhanidhisonii@users.noreply.github.com>
2025-05-30 15:27:50 +0530

    3rd changes

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
 
+app.set('view engine', 'ejs')  
+app.set('views', path.join(__dirname, 'views'))  
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('index', { title:'ejs app',message: 'Hello ejs' }) 
 })
 
 app.listen(port, () => {
